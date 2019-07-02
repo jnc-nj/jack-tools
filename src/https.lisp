abeb6ce46638d770a82e0bda5e6360d3443b1e65
Jack Chou <jacknchou@icloud.com>
2019-07-02 19:16:24 +0800

    added new condiion for encode-http-body

--- src/https.lisp
+++ src/https.lisp
@@ -15,7 +15,8 @@
 	     (coerce body '(vector (unsigned-byte 8))))))))
 
 (defun encode-http-body (body)
-  (cond ((stringp body) body)
+  (cond ((null body) "{}")
+	((stringp body) body)
 	((alistp body) (jonathan:to-json body :from :alist))
 	(t (jonathan:to-json body))))
 
